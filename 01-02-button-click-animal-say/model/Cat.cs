b83ae737f585ebb691975792afa19e716925eafc
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-03-19 09:28:24 +0100

    S2.01 Macska osztály

---
+++ 01-02-button-click-animal-say/model/Cat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_02_button_click_animal_say.model
+{
+    /// <summary>
+    /// S2.01 Macska osztály - a macska köszinti gazdáját
+    /// </summary>
+    class Cat
+    {
+
+        /// <summary>
+        /// A macska neve
+        /// </summary>
+        public string name;
+        /// <summary>
+        /// A macska így köszönti gazdáját
+        /// </summary>
+        public string greet;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="name">A macska neve</param>
+        public Cat(string name)
+        {
+            this.name = name;
+            this.greet = " meow meow";
+        }
+
+        /// <summary>
+        /// A macska köszöntése stringként
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string output = name + " cat says: " + greet;
+            return output;
+        }
+    }
+}
